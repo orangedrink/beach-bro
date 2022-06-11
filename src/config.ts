77c4e1a0abca78c21f58d7a7fe2bf65fd5cd2b4f
orangedrink <orangedrink@users.noreply.github.com>
2022-06-11 14:05:07 -0500

    Whole game lol

---
+++ src/config.ts
@@ -0,0 +1,14 @@
+import Phaser from 'phaser';
+
+export default {
+  type: Phaser.AUTO,
+  parent: 'game',
+  backgroundColor: '#478fc4',
+  scale: {
+    width: 800,
+    height: 600,
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH
+  }
+  
+};
